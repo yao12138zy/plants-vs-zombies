e5a71cf4405988610a39f2a8f4ff0339f22aff7d
yao12138zy <zongyan.yao@mail.utoronto.ca>
2022-10-08 18:21:32 -0400

    Initial commit

---
+++ ref.py
@@ -0,0 +1,21 @@
+import datetime
+import time
+self.start = datetime.datetime.now()
+
+
+end = datetime.datetime.now()
+
+
+class StopWatch():
+    def __init__(self):
+
+
+    def start(self):
+
+    def pause(self):
+
+    def stop(self):
+
+    def tab(self):
+
+
